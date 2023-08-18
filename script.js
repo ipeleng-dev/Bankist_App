7b98c68f07d89e30f33461aeb91ba19f01edd1b3
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-18 15:39:38 +0200

    priject update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Ipeleng Lebelo',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -74,25 +74,89 @@ const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov} €</div>
     </div>
     `
-    containerMovements.insertAdjacentHTML('afterbegin',html)
+    containerMovements.insertAdjacentHTML('afterbegin',html);
 })
 
 }
 
-displayMovements(account1.movements);
+const calcDisplayBalance = function(movements){
+  const balance = movements.reduce((acc,mov) => acc + mov, 0)
 
-/////////////////////////////////////////////////
-// LECTURES
+  labelBalance.textContent = `${balance} €`
+  }
+  
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const calcDisplaySummary = function(acc){
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const incomes = acc.movements.filter(mov => mov > 0)
+  .reduce((acc,mov) => acc + mov, 0);
 
-/////////////////////////////////////////////////
+labelSumIn.textContent = `${incomes} €`
+
+const out = acc.movements.filter(mov => mov < 0)
+  .reduce((acc, mov) => acc + mov, 0);
+
+labelSumOut.textContent = `${Math.abs(out)} €`
+
+const interest = acc.movements
+  .filter(mov => mov > 0)
+  .map(deposit => (deposit * acc.interestRate) / 100)
+  .filter((int, i ,arr) => { 
+    return int >= 1;
+  })
+  .reduce((acc, int) => acc + int, 0);
+
+labelSumInterest.textContent = `${interest} €`
+
+}
+
+
+
+const createUsernames = (accs) => {
+
+  accs.forEach( acc => {
+    
+    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('')
+    
+  });
+}
+
+createUsernames(accounts);
+
+// Event Handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function(e){
+  //prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find( acc => acc.username === inputLoginUsername.value);
+
+  console.log(currentAccount);
+
+  if(currentAccount?.pin === Number(inputLoginPin.value)){
+
+    //display ui and message
+    labelWelcome.textContent =`Welcome back, ${currentAccount.owner.split(' ')[0]}`
+    containerApp.style.opacity = 100;
+
+    //clear input fields
+    inputLoginPin.value = inputLoginUsername.value = '';
+    inputLoginPin.blur();
+
+    //display movements
+    displayMovements(currentAccount.movements);
+
+    //display balance
+    calcDisplayBalance(currentAccount.movements);
+    //display summary
+    calcDisplaySummary(currentAccount);
+
+    //console.log('login')
+
+  }
+  
+})
